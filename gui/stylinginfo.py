3ac42a3ef3450078bcdaa09d1cedcaac45be57dc
inkmedusae <you@example.com>
2025-11-08 18:28:48 +0200

    GUI 1.01
    Added layout for sign up / log in page

---
+++ gui/stylinginfo.py
@@ -0,0 +1,27 @@
+loginbutton_style = """
+            QPushButton {
+                background-color: #4CAF50;
+                color: white;
+                border: none;
+                border-radius: 5px;
+                font-size: 16px;
+                font-weight: bold;
+            }
+            QPushButton:hover {
+                background-color: #45a049;
+            }
+        """
+
+signupbutton_style = """
+            QPushButton {
+                background-color: #2196F3;
+                color: white;
+                border: none;
+                border-radius: 5px;
+                font-size: 16px;
+                font-weight: bold;
+            }
+            QPushButton:hover {
+                background-color: #0b7dda;
+            }
+        """
